b86b4b1ea75122d5b310ceb7b09221b5e5a82dc2
asliddintursunoff <asliddintursunoff2@icloud.com>
2025-10-12 23:15:00 +0500

    fixing pydantic settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,11 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Order Service"
     DATABASE_URL: str = "sqlite:///./orders.db"
 
     class Config:
-        env_file = ".env"   # specify .env file
+        env_file = ".env"
         env_file_encoding = "utf-8"
 
 settings = Settings()
