ee399462a2409ef252fa65ff022137bc9676e3bc
asliddintursunoff <asliddintursunoff2@icloud.com>
2025-10-12 23:33:35 +0500

    3

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,15 @@
+import os
 from pydantic_settings import BaseSettings
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # app/core -> app
+ENV_FILE = os.path.join(BASE_DIR, ".env")  # points to app/.env
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Order Service"
     DATABASE_URL: str = "sqlite:///./orders.db"
 
     class Config:
-        env_file = ".env"
+        env_file = ENV_FILE
         env_file_encoding = "utf-8"
 
 settings = Settings()
